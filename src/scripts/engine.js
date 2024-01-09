d5056d9362e208483c68eab809a26eb6a57dcc6b
szlima <suzanelima@outlook.com>
2024-01-09 11:24:22 -0300

    Aplicando efeitos quando o usuário acerta o inimigo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -27,6 +27,12 @@ function countDown(){
     }
 };
 
+function playSound(audioName){
+    let audio= new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume= 0.2;
+    audio.play();
+};
+
 function randomSquare(){
     state.view.squares.forEach(square =>
         square.classList.remove("enemy")
@@ -45,6 +51,7 @@ function addListenerHitBox(){
                 state.values.result++;
                 state.view.score.textContent= state.values.result;
                 state.values.hitPosition= null;
+                playSound("hit");
             }
         });
     });
